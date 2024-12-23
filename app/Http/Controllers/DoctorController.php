8fbe9ceb28952ad3b5b1913c312d51fddeed3589
aliavrio <ali@gmail.com>
2024-12-23 17:53:46 +0500

    Doctor extends Model

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -30,7 +30,6 @@ class DoctorController extends Controller
      */
     public function store(Request $request)
     {
-<<<<<<< HEAD
         // Validate the request according to the updated schema
         $request->validate([
             'name' => 'required|string|max:255',
@@ -111,22 +110,6 @@ class DoctorController extends Controller
         return $imgUrl;
     }
     
-=======
-        $request->validate(
-            [
-                'name'=>'required',
-                'phone'=> 'required',
-                'speciality'=> 'required',
-                'time'=> 'required',
-                'day'=>'required',
-                'image'=>'required',
-                'fee'=>'required|numeric',
-            ]
-        );
-        Doctor::saveDoctor($request);
-        return back()->with('message','Doctor Added Successfully');
-    }
->>>>>>> 4d0cdcbbad56ca35cf2064831b8fbef357107b1f
 
     /**
      * Display the specified resource.
@@ -152,7 +135,6 @@ class DoctorController extends Controller
      */
     public function update(Request $request, string $id)
     {
-<<<<<<< HEAD
         $request->validate([
             'name' => 'required|string|max:255',
             'visit_days' => 'required|string',
@@ -227,12 +209,6 @@ class DoctorController extends Controller
 
     }
     
-=======
-        Doctor::updateDoctor($request,$id);
-
-        return redirect('doctor');
-    }
->>>>>>> 4d0cdcbbad56ca35cf2064831b8fbef357107b1f
 
     /**
      * Remove the specified resource from storage.
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
     protected $fillable = [
         'category_name',
         'status'
@@ -21,17 +20,6 @@ class Category extends Model
     //     self::$category->status =$request->status;
     //     self::$category->save();
     // }
-=======
-
-    private static $category;
-    use HasFactory;
-    public static function saveCategory($request){
-        self::$category = new Category();
-        self::$category->category_name = $request->category_name;
-        self::$category->status =$request->status;
-        self::$category->save();
-    }
->>>>>>> 4d0cdcbbad56ca35cf2064831b8fbef357107b1f
 
     public static function updateCategory($request,$id){
         self::$category = Category::find($id);
